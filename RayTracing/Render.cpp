dfeb4239a224a7bda60f66a750bac00cf7fc1a71
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-05-20 21:34:52 +0800

    可视化了法线和添加了一个照明

---
+++ RayTracing/Render.cpp
@@ -0,0 +1,97 @@
+#include"Render.h"
+#include"Walnut/Random.h"
+#include"Walnut/Timer.h"
+
+namespace Utils
+{
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+		
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	glm::vec3 rayOrigin(0.0f,0.0f,2.0f);
+	float radius = 1.0f;
+	//rayDirection = glm::normalize(rayDirection);
+
+	//at^2 + bt + c = 0
+	//(bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
+	// a = ray origin
+	// b = ray direction
+	// r = radius
+	// t = hit distance
+	//float a = rayDirection.x * rayDirection.x + rayDirection.y * rayDirection.y + rayDirection.z * rayDirection.z;
+
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	//b^2 - 4ac
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant < 0.0f)
+		return glm::vec4(0,0,0,1);
+
+	//(-b +- sqrt(discriminant)) / 2a
+	float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	//glm::vec3 h0 = rayOrigin + rayDirection * t0;
+	glm::vec3 hitpoint = rayOrigin + rayDirection * closestT;
+	
+	glm::vec3 normal = glm::normalize(hitpoint); //hitpoint - shpereCenter
+	glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));//light
+	float d = glm::max(glm::dot(normal, -lightDir), 0.0f);// = cos(angle);
+	//float d = glm::dot(normal, -lightDir);
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= d;
+	//sphereColor = (normal * 0.5f + 0.5f)*d;
+	return glm::vec4(sphereColor, 1.0f);
+}
+
+void Renderer::Render()
+{
+	//render every pixel
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
+	{
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+		{
+			float aspectRation = m_FinalImage->GetWidth() /(float) m_FinalImage->GetHeight();
+			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(),(float)y / (float)m_FinalImage->GetHeight() };
+			coord = coord * 2.0f - 1.0f; //-1 -> 1
+			coord.x *= aspectRation;
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y*m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
+		}
+	}
+	m_FinalImage->SetData(m_ImageData);
+}
+
+void Renderer::OnResize(uint32_t width, uint32_t height)
+{
+	if (m_FinalImage)
+	{
+		//No resize necessary
+		if (m_FinalImage->GetWidth() == width && m_FinalImage->GetHeight() == height)
+			return;
+		m_FinalImage->Resize(width, height);
+	}
+	else
+	{
+		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
+	}
+	delete[]m_ImageData;
+	m_ImageData = new uint32_t[width * height];
+}
+
